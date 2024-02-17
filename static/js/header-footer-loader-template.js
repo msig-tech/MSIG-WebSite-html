7de7ddf6b918460fd11c279c9f28f596193823d4
Kevin <kevin@moneysmartinvestmentgroup.com>
2024-02-17 10:02:25 -0500

    add google translate widget, start translation to english

--- static/js/header-footer-loader-template.js
+++ static/js/header-footer-loader-template.js
@@ -1,12 +1,30 @@
-function includeHTML(file, targetId) {
-    fetch(file)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(targetId).innerHTML = data;
-        })
-        .catch(error => console.error('Erreur de chargement du fichier:', error));
+document.addEventListener("DOMContentLoaded", function() {
+    // Fonction pour inclure le header et le footer
+    function includeHTML(file, targetId) {
+        return fetch(file)
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById(targetId).innerHTML = data;
+            })
+            .catch(error => console.error('Erreur de chargement du fichier:', error));
+    }
+    
+    // Inclure le header et le footer
+    Promise.all([
+        includeHTML('../../header-template.html', 'header'),
+        includeHTML('../../footer-template.html', 'footer')
+    ]).then(() => {
+        // Manipulations supplémentaires si nécessaire
+    }).catch(error => console.error('Erreur lors du chargement des fichiers HTML:', error));
+});
+
+
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        pageLanguage: 'fr', 
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+        autoDisplay: false,
+        exclude: ['.notranslate']
+    }, 'google_translate_element');
 }
-
-includeHTML('../../header-template.html', 'header');
-
-includeHTML('../../footer-template.html', 'footer')
--- static/js/header-footer-loader.js
+++ static/js/header-footer-loader.js
@@ -1,12 +1,31 @@
-function includeHTML(file, targetId) {
-    fetch(file)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(targetId).innerHTML = data;
-        })
-        .catch(error => console.error('Erreur de chargement du fichier:', error));
+// Ajoutez tout votre contenu JavaScript dans cet événement
+document.addEventListener("DOMContentLoaded", function() {
+    // Fonction pour inclure le header et le footer
+    function includeHTML(file, targetId) {
+        return fetch(file)
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById(targetId).innerHTML = data;
+            })
+            .catch(error => console.error('Erreur de chargement du fichier:', error));
+    }
+    
+    // Inclure le header et le footer
+    Promise.all([
+        includeHTML('header.html', 'header'),
+        includeHTML('footer.html', 'footer')
+    ]).then(() => {
+        // Manipulations supplémentaires si nécessaire
+    }).catch(error => console.error('Erreur lors du chargement des fichiers HTML:', error));
+});
+
+
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        pageLanguage: 'fr', 
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+        autoDisplay: false,
+        exclude: ['.notranslate']
+    }, 'google_translate_element');
 }
-
-includeHTML('header.html', 'header');
-
-includeHTML('footer.html', 'footer')
